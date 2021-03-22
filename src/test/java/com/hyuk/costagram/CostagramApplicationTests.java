575a9e75c40c063d2307ef3bf53eed34a48109d7
chmh0805 <chmh0805@gmail.com>
2021-03-22 12:19:10 +0900

    1.인스타그램 클론 - UI 완료

---
+++ src/test/java/com/hyuk/costagram/CostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuk.costagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
